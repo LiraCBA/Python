461003538132908a0f3e3465fecf742d6d25094d
LiraCBA <lirinhaduml@gmail.com>
2024-10-09 14:15:18 -0300

    Add files via upload

---
+++ 14-par-ou-impar.py
@@ -0,0 +1,19 @@
+def par(x):
+    return x % 2 == 0
+
+
+def par_ou_impar(x):
+    if par(x):
+        return "par"
+    else:
+        return "impar"
+    
+
+valor = 0
+while valor != 'S':
+    valor = input("Digitte um valor ou 'S' para sair: ")
+    if valor != 'S':
+        print(par_ou_impar(int(valor)))
+    else:
+        print("Fim do programa")
+
